0dd1cefdbce3810ebba3800ba6410975feac5921
Fibonacci1664 <dave.green1664@yahoo.co.uk>
2019-06-12 13:01:39 +0100

    First commit

---
+++ src/Cat.cpp
@@ -0,0 +1,25 @@
+/*
+ * Cat.cpp
+ *
+ *  Created on: 12 Jun 2019
+ *      Author: Dave
+ */
+
+#include<iostream>
+#include "Cat.h"
+
+using namespace std;
+
+void Cat::speak()
+{
+	cout << "Meow!" << "\n";
+}
+
+void Cat::jump()
+{
+	cout << "Jumping" << "\n";
+}
+
+
+
+
---
+++ src/Classes.cpp
@@ -0,0 +1,26 @@
+/*
+ * Classes.cpp
+ *
+ *  Created on: 12 Jun 2019
+ *      Author: Dave
+ */
+
+#include<iostream>
+#include"Cat.h"
+
+using namespace std;
+
+int main()
+{
+	Cat cat;
+
+	cat.speak();
+	cat.jump();
+
+
+
+
+	return 0;
+}
+
+
